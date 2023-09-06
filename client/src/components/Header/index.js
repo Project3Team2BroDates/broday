326e06e4c2b9170d879f2dd841a65357aec40533
Forbes85 <vudo8585@gmail.com>
2023-09-06 14:51:59 -0500

    imported bulma framework

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
+    <header className="bg-danger text-dark mb-4 py-3 display-flex align-center">
       <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
         <h1 className="m-0" style={{ fontSize: '3rem' }}>
           Tech Friends
