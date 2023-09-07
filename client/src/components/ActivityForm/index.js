ac2f4cc69fc73273186a11e2efb535f080084814
teylorsmith34 <teylorsmith34@gmail.com>
2023-09-07 17:36:48 -0400

    Deleted Profile.js

--- client/src/components/ActivityForm/index.js
+++ client/src/components/ActivityForm/index.js
@@ -1,82 +1,81 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
 
-import { ADD_ACTIVITY } from '../../utils/mutations';
+import { ADD_ACTIVITY } from "../../utils/mutations";
 // import { QUERY_ACTIVITIES } from '../../utils/queries';
 
-import Auth from '../../utils/auth';
+import Auth from "../../utils/auth";
 
 const ActivityForm = () => {
-    const [activityText, setActivityText] = useState('');
-    
-    const [addActivity, {error}] = useMutation(ADD_ACTIVITY);
-  
+  const [activityText, setActivityText] = useState("");
 
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        
-        try {
-          console.log(activityText);
-            const { data } = await addActivity({
-                variables: {activityText},
-            });
+  const [addActivity, { error }] = useMutation(ADD_ACTIVITY);
 
-            setActivityText('');
-        } catch (err) {
-            console.error(err);
-        }
-    };
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
+    try {
+      console.log(activityText);
+      const { data } = await addActivity({
+        variables: { activityText },
+      });
 
-        if (name === 'activityText') {
-            setActivityText(value);
-        }
-    };
+      setActivityText("");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-    return (
-        <div>
-        <h3>What activities do you enjoy?</h3>
-  
-        {Auth.loggedIn() ? (
-          <>
-            <form
-              className="flex-row justify-center justify-space-between-md align-center"
-              onSubmit={handleFormSubmit}
-            >
-              <div className="col-12 col-lg-9">
-                <textarea
-                  name="activityText"
-                  placeholder="Here's a new activity..."
-                  value={activityText}
-                  className="form-input w-100"
-                  style={{ lineHeight: '1.5', resize: 'vertical' }}
-                  onChange={handleChange}
-                ></textarea>
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === "activityText") {
+      setActivityText(value);
+    }
+  };
+
+  return (
+    <div>
+      <h3>What activities do you enjoy?</h3>
+
+      {Auth.loggedIn() ? (
+        <>
+          <form
+            className="flex-row justify-center justify-space-between-md align-center"
+            onSubmit={handleFormSubmit}
+          >
+            <div className="col-12 col-lg-9">
+              <textarea
+                name="activityText"
+                placeholder="Here's a new activity..."
+                value={activityText}
+                className="form-input w-100"
+                style={{ lineHeight: "1.5", resize: "vertical" }}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+
+            <div className="col-12 col-lg-3">
+              <button className="btn btn-primary btn-block py-3" type="submit">
+                Add Activity
+              </button>
+            </div>
+            {error && (
+              <div className="col-12 my-3 bg-danger text-white p-3">
+                {error.message}
               </div>
-  
-              <div className="col-12 col-lg-3">
-                <button className="btn btn-primary btn-block py-3" type="submit">
-                  Add Activity
-                </button>
-              </div>
-              {error && (
-                <div className="col-12 my-3 bg-danger text-white p-3">
-                  {error.message}
-                </div>
-              )}
-            </form>
-          </>
-        ) : (
-          <p>
-            You need to be logged in to add an activity. Please{' '}
-            <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
-          </p>
-        )}
-      </div> 
-    );
+            )}
+          </form>
+        </>
+      ) : (
+        <p>
+          You need to be logged in to add an activity. Please{" "}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
+    </div>
+  );
 };
 
 export default ActivityForm;
