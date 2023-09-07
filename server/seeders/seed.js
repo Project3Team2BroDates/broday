caa93ad1fbd8b84d7c8dba61d278ab62b3355019
Peter Conenna <pconenna@depaul.edu>
2023-09-07 16:40:14 -0500

    seeders

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,9 +1,23 @@
 const db = require('../config/connection');
-const { User } = require('../models');
+const { User, Activity } = require('../models');
 const UserSeeds = require('./userSeeds.json');
-
+const activitySeeds = require('./activitySeeds.json');
 db.once('open', async () => {
   try {
+    await Activity.deleteMany({});
+    await Activity.insertMany([
+      { "activityText": "Football"},
+      {"activityText": "Golf"},
+      {"activityText": "Video Games"},
+      {"activityText": "Soccer"},
+      {"activityText": "Fishing"},
+      {"activityText": "Baseball"},
+      {"activityText": "Baseketball"},
+      {"activityText": "Hunting"},
+      {"activityText": "Hiking"},
+      {"activityText": "Working Out"}
+  ])
+    console.log("Activites seeded!");
     await User.deleteMany({});
     await User.create(UserSeeds);
 
